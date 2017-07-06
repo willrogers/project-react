dd1a2a5e76e3481bec7d03ed253067e279e8f2da
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-07-06 15:05:28 +0100

    Extend functionality of HelloUser to be more interactive

--- src/HelloUser.js
+++ src/HelloUser.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
+//The HelloUser component: Displays the username in a Hello World.
 var HelloUser = React.createClass({
 	
+	//Set the initial state: empty to begin with 
 	getInitialState: function(){
 		return {
-			name:'Benedict Wagnall'
+			name:''
 		}
 	},
 
+//Sets the name prop/state to the supplied parameter - (e)
+	handleChange: function(e){
+		this.setState({
+			name: e.target.value
+		});
+	},
+
+
+//Produces an input box that, when a user puts data in, handleChange is called
 	render:function(){
 		return(
-			<div> Hello {this.state.name}  </div>
+		<div> Hello {this.state.name} <br/>
+			Enter Your name here:<br/>
+			<input type="text" name={this.state.name} onChange={this.handleChange} />
+		</div>
 
 		)		
 	}
---
+++ src/ShoppingList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+var ShoppingList = 
+
+export default ShoppingList;
