d90ed61ba963d54010347f5e10179f5dec0ccb24
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-07-28 16:17:32 +0100

    Touch up connection

--- src/connection.js
+++ src/connection.js
@@ -1,7 +1,9 @@
 
 
-
+  function fireConnection()
+  {
   var connection = new WebSocket('ws://localhost:8080/ws')
+  }
 
   connection.onopen = function()
   {
@@ -16,7 +18,8 @@
     connection.send(unsubRequest)
   }
 
-  connection.onMesssage = function(event){
+  connection.onMesssage = function(event)
+  {
     var malcResponse = JSON.parse(event.data)
 
     SignalComponent.receive(response.value.meta.label)
