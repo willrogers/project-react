58ff97391b53f416ff297b8f67abd85ab4d9e591
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-07-31 16:45:59 +0100

    Running test with travis

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {shallow, mount, render} from 'enzyme'
 import SignalComponent from './signalComponent'
+var expect = require('chai').expect
+
 
 describe('the Signal Component div ' , function () {
     it('takes no props', function () {
