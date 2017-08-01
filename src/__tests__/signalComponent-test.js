50ec776f4a37c18f135a863a85868c4ef3eff148
Will Rogers <wahrogers@gmail.com>
2017-08-01 16:52:50 +0100

    Write passing test using karma, chai and enzyme.

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -4,8 +4,10 @@ import SignalComponent from '../SignalComponent.js'
 var expect = require('chai').expect
 
 
-describe('the Signal Component div ' , function () {
+describe('The SignalComponent div' , function () {
     it('takes no props', function () {
-        expect(Object.keys(SignalComponent.props()).length).toBe(0)
+        var s = new SignalComponent({})
+        console.log(s)
+        expect(Object.keys(s.props).length).to.equal(0)
     })
 })
