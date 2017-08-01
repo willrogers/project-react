ef3974e90bec1a0529fb18b1d2a0b4555f8b03b1
Will Rogers <wahrogers@gmail.com>
2017-08-01 16:56:50 +0100

    Tidy test file.

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import {shallow, mount, render} from 'enzyme'
 import SignalComponent from '../SignalComponent.js'
 var expect = require('chai').expect
 
@@ -7,7 +5,6 @@ var expect = require('chai').expect
 describe('The SignalComponent div' , function () {
     it('takes no props', function () {
         var s = new SignalComponent({})
-        console.log(s)
         expect(Object.keys(s.props).length).to.equal(0)
     })
 })
