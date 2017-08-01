0dcd460c50bf268b87b317aad49bd71ccc3c5b9f
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-08-01 09:29:28 +0100

    ESlint fixes

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import {React} from 'react'
 import {shallow, mount, render} from 'enzyme'
-import SignalComponent from './signalComponent'
+import {SignalComponent} from './SignalComponent.js'
 var expect = require('chai').expect
-
 
 describe('the Signal Component div ' , function () {
     it('takes no props', function () {
