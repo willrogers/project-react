cf4adc59e78827a8fc4eddbe90c5d88c02c873f2
Will Rogers <wahrogers@gmail.com>
2017-08-01 16:52:34 +0100

    Tidy Karma config.

--- karma.conf.js
+++ karma.conf.js
@@ -9,29 +9,15 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
         {pattern: 'tests.webpack.js'},
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    plugins: [ 'karma-mocha', 'karma-webpack', 'karma-firefox-launcher', 'karma-babel-preprocessor'
-    ],
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-
-      preprocessors: {'src/**/*.js': [ 'webpack', 'babel'],
-                      'tests.webpack.js': [ 'webpack' ]},
+    preprocessors: {'src/**/*.js': [ 'webpack', 'babel'],
+                    'tests.webpack.js': [ 'webpack' ]},
 
     babelPreprocessor: {
         options: {
@@ -79,19 +65,14 @@ module.exports = function(config) {
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
