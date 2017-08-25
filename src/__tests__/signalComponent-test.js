bc36e33561c68019be4b77b28c459470812fee1f
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-08-25 15:14:14 +0100

    Begin test fixes

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -1,19 +1,21 @@
-import SignalComponent from '../SignalComponent.js';
+//import SignalComponent from '../SignalComponent.js';
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
 
 describe('SignalComponent' , function() {
+
+
     it('takes no props', function() {
         var sigComp = new SignalComponent({});
         expect(Object.keys(sigComp.props).length).to.equal(0);
     });
 
-    it('should have a receiveUpdate function', function(){
+     it('should have a receiveUpdate function', function(){
         var sigComp = new SignalComponent({});
         expect(sigComp.receiveUpdate).to.be.a('function');
 
-    });
+     });
 
     it('Should contain the signal state', function(){
         var sigComp = new SignalComponent({});
