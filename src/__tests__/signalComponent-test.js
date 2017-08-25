1b6981e5bccbdf3b2566158300a57e042dcea320
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-08-25 16:02:04 +0100

    Add work in progress shallowrender test for EPICSComp

--- src/__tests__/signalComponent-test.js
+++ src/__tests__/signalComponent-test.js
@@ -1,31 +1,33 @@
-//import SignalComponent from '../SignalComponent.js';
+import {EPICSComponent} from '../EPICSComponent.js';
+// import DivComponent from '../DivComponent.js';
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from 'chai';
 
-describe('SignalComponent' , function() {
-
-
-    it('takes no props', function() {
-        var sigComp = new SignalComponent({});
-        expect(Object.keys(sigComp.props).length).to.equal(0);
-    });
-
+describe('EPICSComponent' , function() {
      it('should have a receiveUpdate function', function(){
-        var sigComp = new SignalComponent({});
-        expect(sigComp.receiveUpdate).to.be.a('function');
+        var EPComp = new EPICSComponent({});
+        expect(EPComp.receiveUpdate).to.be.a('function');
 
      });
 
-    it('Should contain the signal state', function(){
-        var sigComp = new SignalComponent({});
-        expect(sigComp.state.signal).to.exist;
+    it('Should start with a null EPICSValue state', function(){
+        var EPComp = new EPICSComponent({});
+        expect(EPComp.state.EPICSValue).to.be.null;
     });
 
     it('Should be able to receive a response and update state accordingly', function(){
-        var wrapper = shallow(<SignalComponent/>);
-        wrapper.instance().receiveUpdate(4);
-        expect(wrapper.instance().state.signal).to.equal(4);
+        var EPComp = shallow(<EPICSComponent/>);
+        EPComp.receiveUpdate(4);
+        expect(EPComp.state.EPICSValue).to.equal(4);
     });
-
 });
+
+// describe('DivComponent' , function(){
+//     it('Should be able to receive a response and update state accordingly', function(){
+//         // var wrapper = shallow(<DivComponent/>);
+//         // wrapper.instance().receiveUpdate(4);
+//         // expect(wrapper.instance().state.EPICSValue).to.equal(4);
+//     });
+
+// });
