24a57d1f1fcc90deb36e943e660cf4d24ceb3789
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-08-31 10:57:16 +0100

    Refactor for empty render method in EPICSComponent. Rename test file to something more current

---
+++ src/__tests__/epicsComponent-test.js
@@ -0,0 +1,33 @@
+import {EPICSComponent} from '../EPICSComponent.js';
+import {DivComponent} from '../DivComponent.js';
+/*eslint-disable no-unused-vars*/
+import React from 'react';
+/*eslint-disable no-unused-vars*/
+import {shallow, mount} from 'enzyme';
+import {expect} from 'chai';
+
+describe('EPICSComponent' , function() {
+    it('should have a receiveUpdate function', function(){
+        var EPComp = new EPICSComponent({});
+        expect(EPComp.receiveUpdate).to.be.a('function');
+
+    });
+
+    it('Should start with a null EPICSValue state', function(){
+        var EPComp = new EPICSComponent({});
+        expect(EPComp.state.EPICSValue).to.be.null;
+    });
+
+    it('Should be able to receive a response and update state accordingly', function(){
+        var EPComp = shallow(<EPICSComponent/>);
+        EPComp.instance().receiveUpdate(4);
+        expect(EPComp.instance().state.EPICSValue).to.equal(4);
+    });
+});
+
+describe('DivComponent' , function(){
+    it('Should render a div', function(){
+        var DivComp = shallow(<DivComponent/>);
+        expect(DivComp.type()).to.equal('div');
+    });
+});
