54e7cdcc3a1edc8736289f0ae80c3d91ba57a911
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-09-01 08:48:25 +0100

    Add another test to inputComponent-test.js

--- src/__tests__/inputComponent-test.js
+++ src/__tests__/inputComponent-test.js
@@ -11,14 +11,18 @@ describe('InputComponent' , function(){
         expect(InpComp.type()).to.equal('input');
     });
 
+    it('Should be created with no value', function(){
+        var InpComp = shallow(<InputComponent/>);
+        expect(InpComp.html()).to.equal('<input readonly=""/>');
+
+    });
+
     it('Should update the contents of an input', function(){
         var InpComp = shallow(<InputComponent/>);
         InpComp.instance().receiveUpdate('bar');
+        //The ' readonly="" ' here is a slight deviation from the code,
+        //due to transpilation from ES6 to older standards
         expect(InpComp.html()).to.equal('<input value="bar" readonly=""/>');
     });
-
-    // it('Should be read-only', function(){
-
-    // });
 
 });
