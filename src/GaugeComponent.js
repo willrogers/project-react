c01123ad89823ead8568e638623e10e2fd90f47f
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-09-07 13:55:56 +0100

    Add working  gauge draw to gaugeComponent

--- src/GaugeComponent.js
+++ src/GaugeComponent.js
@@ -18,9 +18,74 @@ export class GaugeComponent extends EPICSComponent{
         super(props);
     }  
 
+
+
     componentDidMount(){
-        var context = this.refs.gaugeRef;
-        console.log(context);
+        const canvas = this.refs.gaugeRef;
+        const context = canvas.getContext('2d');
+        const quarterMark = (canvas.width*0.25);
+        const halfMark = (canvas.width*0.5);
+        const threeQaurterMark = (canvas.width*0.75);
+        const pipSize = (canvas.width/200);    
+
+        for(var i=0 ; i<canvas.width ; i+=pipSize){
+
+            if(i==quarterMark){
+                this.drawMarker(i, context);
+                this.annotateMarker(i, context);
+
+            } else if (i==halfMark){
+                this.drawMarker(i, context);
+                this.annotateMarker(i, context);
+
+            } else if (i==threeQaurterMark){ 
+                this.drawMarker(i, context);
+                this.annotateMarker(i, context);
+
+            } else {
+                this.drawPip(i, context);
+            }
+        }
+    
+          
+    }
+
+    
+
+
+    annotateMarker(annoLoc, ctx){
+console.log("annotate called");
+        ctx.fillText(""+(annoLoc*0.2)+"", annoLoc, 140);
+    }
+
+    drawPip(pipLoc, ctx){
+console.log("draw pip called");
+        ctx.beginPath();
+        ctx.lineWidth="0.5";
+        ctx.strokeStyle="#000000";
+        ctx.moveTo(pipLoc, 100);
+        ctx.lineTo(pipLoc, 130);
+        ctx.stroke();
+    }
+
+    drawMarker(markerLoc, ctx){
+console.log("draw marker called");
+        ctx.beginPath();
+        ctx.lineWidth="1";
+        ctx.strokeStyle="#000000";
+        ctx.moveTo(markerLoc, 50);
+        ctx.lineTo(markerLoc, 130);
+        ctx.stroke();
+    }
+
+    updateNeedle(epicsVal, ctx){
+console.log("update needle called");
+        ctx.beginPath();
+        ctx.lineWidth="3";
+        ctx.strokeStyle="#ff0000";
+        ctx.moveTo((epicsVal*5), 130);
+        ctx.lineTo((epicsVal*5), 10);
+        ctx.stroke();
     }
 
 
