6b3e1029a48497ee5197266f2e406e1eca272d67
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-09-12 14:28:54 +0100

    update input control Attempt writer component tests

---
+++ src/__tests__/writerComponent-test.js
@@ -0,0 +1,23 @@
+import {WriterComponent} from '../WriterComponent.js';
+/*eslint-disable no-unused-vars*/
+import React from 'react';
+/*eslint-disable no-unused-vars*/
+import {shallow, mount} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+describe('' , function(){
+    it('', function(){
+        
+        // var WrtComp = shallow(<WriterComponent/>);
+        // const inputEvent = {target: {value:'45'}, preventDefault: () => {} };
+        // WrtComp.instance().handleSubmit(inputEvent);
+        // const inputEvent = {target: {value:'45'}, preventDefault: () => {} };
+        // const WrtComp = shallow(<WriterComponent/>);
+        // const EpicsStub = sinon.stub(WrtComp);
+
+        // WrtComp.find('form').simulate('submit', [inputEvent, EpicsStub]);
+
+        
+    });
+});
