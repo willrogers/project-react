1ad232fac196654bbffe49ab1d6d7657623615fd
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-09-13 12:38:43 +0100

    Working test using spies for LEDComponent

--- src/LEDComponent.js
+++ src/LEDComponent.js
@@ -12,7 +12,7 @@ const defaultFill = '#000000';
 const redFill= '#FF0000';
 const yellowFill = '#FFFF00';
 const greenFill = '#00FF00';
-const errorFill = '#ff00cb';
+const errorFill = '#FF00CB';
 
 
 
@@ -32,6 +32,8 @@ export class LEDComponent extends EPICSComponent{
     componentDidMount(){
 
         //Initialise the canvas and context, must be done after mount to access ref.
+        console.log("componentDidMount called");
+
         this.canvas = this.refs.ledRef;
         this.context = this.canvas.getContext('2d');
         super.componentDidMount();
@@ -42,6 +44,8 @@ export class LEDComponent extends EPICSComponent{
 
     //On update, redraw the circle with the corresponding colour 
     componentDidUpdate(){
+
+         console.log("componentDidUpdate called");
 
         if(this.state.EPICSValue == 0){
             this.drawCircle(redFill);
@@ -60,6 +64,8 @@ export class LEDComponent extends EPICSComponent{
 
     //Draws the LED with the supplied colour
     drawCircle(colour){
+
+        console.log("drawCircle called with " + colour);
 
         this.context.beginPath();
         this.context.arc(75,75,60,0,2*Math.PI);
---
+++ src/__tests__/ledComponent-test.js
@@ -0,0 +1,50 @@
+import {LEDComponent} from '../LEDComponent.js';
+/*eslint-disable no-unused-vars*/
+import React from 'react';
+/*eslint-disable no-unused-vars*/
+import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
+import {expect} from 'chai';
+
+describe('LEDComponent' , function(){
+
+    it('should render a canvas', function(){
+        var LED = shallow(<LEDComponent/>);
+        expect(LED.type()).to.equal('canvas');
+    });
+
+    it('should draw a correctly coloured circle based on input value', function(){});
+        
+        var LEDComp = mount(<LEDComponent/>);
+        var LEDSpy = sinon.spy(LEDComp.instance(), "drawCircle");
+
+
+        console.log("------------------------------------");
+        console.log("State updated with 3");
+        LEDComp.instance().setState({EPICSValue: '3'});
+        expect(LEDSpy.calledOnce).to.equal(true);
+        expect(LEDComp.instance().drawCircle.getCall(0).args[0]).to.equal('#FF00CB');
+        console.log("EpicsValue is now "+LEDSpy.firstCall.thisValue.state.EPICSValue);
+
+        console.log("------------------------------------");
+
+        console.log("State updated with 0");
+        LEDComp.instance().setState({EPICSValue: '0'});
+        expect(LEDSpy.calledTwice).to.equal(true);
+        expect(LEDComp.instance().drawCircle.getCall(1).args[0]).to.equal('#FF0000');
+        console.log("EpicsValue is now "+LEDSpy.secondCall.thisValue.state.EPICSValue);
+
+        console.log("------------------------------------");
+
+            console.log("LEDSpy is called "+ LEDSpy.callCount +" times");
+            console.log("LEDComp is called "+ LEDComp.instance().drawCircle.callCount +" times");
+            
+            console.log("------------------------------------");
+
+            console.log("Arguments passed to drawCircle in order:")
+            console.log(LEDSpy.getCall(0).args);
+            console.log(LEDSpy.getCall(1).args);
+            
+    it('', function(){});
+
+});
