6ebeb89b41f1df530fc9e7dd1ea827422bcc8af4
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-09-20 09:37:47 +0100

    Gauge background and some logic complete

--- src/RadialGaugeComponent.js
+++ src/RadialGaugeComponent.js
@@ -4,8 +4,12 @@ import React from 'react';
 /*eslint-disable no-unused-vars*/
 import {EPICSComponent} from './EPICSComponent.js';
 
+const EMin = null;
+const EMax = null;
+const radialDegreeRange = 300;
+const oneHundred = 100;
 
-export class GaugeComponent extends EPICSComponent{
+export class RadialGaugeComponent extends EPICSComponent{
 
     constructor(props){
         super(props);
@@ -15,19 +19,116 @@ export class GaugeComponent extends EPICSComponent{
         //definitions()
         //drawGauge()
         this.svg = this.refs.radialRef;
+
     }
 
     componentDidUpdate(){
-        //drawNeedle()    }
+        drawNeedle(calculateNeedleLocation());
+    }
 
 
     drawGauge(){
 
     }
 
-    render(){}
-        return(<svg ref='radialRef'>
+    drawNeedle(){
+
+    }
+
+    //Take the incoming epics value and convert it to a point on the
+    // dial. Converts the percentage of the max of the given epicsVal,
+    // as a percentage value of the radialGauge.
+    calculateNeedleLocation(epicsVal){
+
+        //Calculate the difference between the lower and upper bounds.
+        //Lower bound will typically be zero.
+        var epicsRange = (EMax-EMin);
+
+        //Calculate one percent of the epicsValue range.
+        var onePercent = (ERange/oneHundred);
+
+        //Convert the supplied epics value into a percentage
+        //of the epicsValue range. 
+        var epicsValAsPercentage = (epicsVal/onePercent);
+
+        //Figure out one percent of the radial gauge
+        var radialOnePercent = (radialDegreeRange/oneHundred);
+
+        //Calculate the needle location.
+        var needleLocation = (radialOnePercent*epicsValAsPercentage);
+
+        return needleLocation;
+    }
 
 
-            </svg>)
+
+    render(){
+        return( <svg ref={'radialRef'}
+                    width={400}
+                    height={400}
+                    clipPath={'url(#myClip)'}>
+
+
+                <defs>
+
+                    <line   id={'pip'}
+                            x1={200} 
+                            y1={50} 
+                            x2={200} 
+                            y2={70}
+                            stroke={'black'}
+                            strokeWidth={5}></line>
+
+                    <line   id={'bigPip'}
+                            x1={200} 
+                            y1={50} 
+                            x2={200} 
+                            y2={80}
+                            stroke={'black'}
+                            strokeWidth={10}></line>
+                </defs>
+
+                <circle     cx={200}
+                            cy={200}
+                            r={150}
+                            fill={'none'}
+                            stroke={'black'}
+                            strokeWidth={6}/>
+
+                <polygon points={'200,200 115,360 285,360'} style={{fill:'white'}}/>
+
+
+                <circle     cx={200} 
+                            cy={200} 
+                            r={18} 
+                            fill={'red'}/>
+                <circle     cx={200} 
+                            cy={200} 
+                            r={12} 
+                            fill={'white'}/>
+                <circle     cx={200} 
+                            cy={200} 
+                            r={10} 
+                            fill={'black'}/>
+
+                <g  id={'pips'}>
+                    <use className={'pip'} href={'#pip'}></use>
+
+                    <use className={'pip'} href={'#pip'} transform={"rotate(-30 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(-60 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(-90 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(-120 200 200)"}></use>
+                    <use className={'bigPip'} href={'#bigPip'} transform={"rotate(-150 200 200)"}></use>
+                    
+                    <use className={'pip'} href={'#pip'} transform={"rotate(30 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(60 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(90 200 200)"}></use>
+                    <use className={'pip'} href={'#pip'} transform={"rotate(120 200 200)"}></use>
+                    <use className={'bigPip'} href={'#bigPip'} transform={"rotate(150 200 200) "}></use>
+                </g>
+              
+
+            </svg>
+        );
+    }
 }
